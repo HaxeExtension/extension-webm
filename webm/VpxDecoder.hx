d704cbb08ee0c8738bbc6fe6ae3e2b5fbf304cec
Casper Smith <casperksmith@gmail.com>
2015-02-07 00:52:58 +1100

    Get frame method redundant

--- webm/VpxDecoder.hx
+++ webm/VpxDecoder.hx
@@ -39,21 +39,6 @@ class VpxDecoder
 		}
 	}
 	
-	public function getFrame():BitmapData 
-	{
-		var info = hx_vpx_codec_get_frame(context);
-		var bitmapData:BitmapData = null;
-		
-		if (info != null)
-		{
-			var byteArray = ByteArray.fromBytes(Bytes.ofData(info[2]));
-			bitmapData = new BitmapData(info[0], info[1]);
-			bitmapData.setPixels(bitmapData.rect, byteArray);
-		}
-		
-		return bitmapData;
-	}
-	
 	static var hx_vpx_codec_iface_name:Void -> String = Lib.load("openfl-webm", "hx_vpx_codec_iface_name", 0);
 	static var hx_vpx_codec_dec_init:Void -> Dynamic = Lib.load("openfl-webm", "hx_vpx_codec_dec_init", 0);
 	static var hx_vpx_codec_decode:Dynamic -> BytesData -> Array<Int> = Lib.load("openfl-webm", "hx_vpx_codec_decode", 2);
